913614c24535d9426c6c25029f8ff0fd39954465
Alaqian <a7zafar@gmail.com>
2023-02-18 20:38:26 -0500

    output file

--- Hadoop/mapper.py
+++ Hadoop/mapper.py
@@ -10,7 +10,9 @@ def read_input(input):
         yield line.split()
 
 
-def main(separator='\t'): # default separator is a tab - which is \t - can change it to something else
+def main(
+    separator="\t",
+):  # default separator is a tab - which is \t - can change it to something else
     # input comes from STDIN (standard input) - read and input from standard input
     data = read_input(sys.stdin)
 
@@ -21,7 +23,7 @@ def main(separator='\t'): # default separator is a tab - which is \t - can chang
         #
         # tab-delimited; the trivial word count is 1
         for word in line:
-            print('%s%s%d' % (word, separator, 1))
+            print("%s%s%d" % (word, separator, 1))
 
 
 # how to test locally in bash/linus: cat <input> | python mapper.py
---
+++ test.py
@@ -0,0 +1,1 @@
+print("The cat in the hat is teh best cat in the hat.\nThe cat in the hat is amazing!")
