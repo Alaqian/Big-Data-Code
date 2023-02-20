550d73323054b194e0c3e5034aa91a464e7027d0
Alaqian <a7zafar@gmail.com>
2023-02-20 01:24:35 -0500

    Update map.py to map bigrams and triagrams

--- Hadoop/map.py
+++ Hadoop/map.py
@@ -4,5 +4,10 @@ import re
 for line in sys.stdin:
     line = re.sub("[^a-zA-Z0-9]", " ", line.lower())
     words = line.split()
-    for word in words:
-        print("%s%s%d" % (word, "\t", 1))
+    words_in_line = len(words)
+    for i in range(words_in_line):
+        print("%s%s%d" % (words[i], "\t", 1))
+        if i < words_in_line - 1:
+        print("%s%s%d" % (words[i] + " " + words[i + 1], "\t", 1))"""
+        if i < words_in_line - 2:
+            print("%s%s%d" % (words[i] + " " + words[i + 1] + " " + words[i + 2], "\t", 1))
