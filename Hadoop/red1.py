3e2047d1f2022d2b1d0df238b93fd15d3c463b69
Alaqian <a7zafar@gmail.com>
2023-02-21 05:19:33 -0500

    Commented Print to stderr

--- Hadoop/red1.py
+++ Hadoop/red1.py
@@ -25,9 +25,8 @@ def main(separator="\t"):
         # dangerous, could run out of memory, loads everything at once.
         try:
             total_count = sum(int(count) for current_word, count in group)
-            if "_" in current_word:
-                pass
-                # print(current_word + " " + str(total_count))
+            # if "_" in current_word:
+            # print(current_word + " " + str(total_count))
             print("%s%s%d" % (current_word, separator, total_count))
         except ValueError:
             # count was not a number, so silently discard this item
