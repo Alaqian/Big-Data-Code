2f52a8f1ee74735767daf61dca91aa7d060f7729
Alaqian <a7zafar@gmail.com>
2023-02-20 19:12:36 -0500

    Add first mapper map1.py

---
+++ Hadoop/map1.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+"""An advanced Mapper, using Python iterators and generators."""
+
+import re
+import sys
+
+
+def read_input(input):
+    for line in input:
+        line = re.sub("[^a-zA-Z0-9]", " ", line.lower()) # Apply regex to replace undesired characters
+        # split the line into words; keep returning each word - for each line in the input, return each line
+        yield line.split()
+
+
+def main(separator='\t'): # default separator is a tab - which is \t - can change it to something else
+    # input comes from STDIN (standard input) - read and input from standard input
+    words = read_input(sys.stdin)
+    words_in_line = len(words) # Count the number of words in the line
+    if words_in_line  > 2: # check if there are atleast 3 words in the line
+        for i in rangeI(words_in_line):
+            # write the results to STDOUT (standard output);
+            # what we output here will be the input for the
+            # Reduce step, i.e. the input for reducer.py
+            #
+            # tab-delimited; the trivial word count is 1
+            print("%s%s%d" % (words[i], "\t", 1)) # individual unigram count
+            print("%s%s%d" % ("unigram_count", "\t", 1)) # total unigram count
+            if i < words_in_line - 1: # bigrams in line = ungrams in line - 1
+                print("%s%s%d" % (words[i] + " " + words[i + 1], "\t", 1)) # individual bigram count
+                print("%s%s%d" % ("bigram_count", "\t", 1)) # total bigram count
+            if i < words_in_line - 2: # trigrams in line = unigrams - 2
+                print("%s%s%d" % (words[i] + " " + words[i + 1] + " " + words[i + 2], "\t", 1)) # individual trigram count
+                print("%s%s%d" % ("trigram_count", "\t", 1)) # total trigram count
+
+
+
+# how to test locally in bash/linus: cat <input> | python mapper.py
+if __name__ == "__main__":
+    main()
